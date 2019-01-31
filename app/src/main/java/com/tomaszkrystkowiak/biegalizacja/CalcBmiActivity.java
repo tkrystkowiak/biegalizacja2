911e9696b2f7cf04f32299a88aed518040935560
Paweł <nowackiads@gmail.com>
2019-01-31 17:03:44 +0100

    Improvements to UI & new info for user

--- app/src/main/java/com/tomaszkrystkowiak/biegalizacja/CalcBmiActivity.java
+++ app/src/main/java/com/tomaszkrystkowiak/biegalizacja/CalcBmiActivity.java
@@ -14,6 +14,7 @@ public class CalcBmiActivity extends Activity {
     private EditText weight_placeholder;
     private TextView bmi_result;
     private TextView bmi_advise;
+    private TextView bmi_diff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class CalcBmiActivity extends Activity {
         weight_placeholder = (EditText) findViewById(R.id.weight_placeholder);
         bmi_result = (TextView) findViewById(R.id.bmi_result);
         bmi_advise = (TextView) findViewById(R.id.bmi_advise);
+        bmi_diff = (TextView) findViewById(R.id.bmi_diff);
     }
 
     public void calculateBmi(View v) {
@@ -38,13 +40,20 @@ public class CalcBmiActivity extends Activity {
             bmi_result.setText(new DecimalFormat("##.##").format(bmi));
 
             if (bmi < 18.50) {
-                bmi_advise.setText("Woah! It's serious underweight. Eat more!");
+                bmi_advise.setText("Woah! It's a serious underweight. Eat more!");
+                float diff = (19 *((heightValue)/100)*((heightValue)/100)) - weightValue;
+                bmi_diff.setText(String.valueOf("You need to gain " + new DecimalFormat("##.#").format(diff) + " kg!"));
             } else if (bmi < 25.00) {
                 bmi_advise.setText("Healthy weight! You are in a good shape - keep going!");
+                bmi_diff.setText("You are on good path!");
             } else if (bmi < 29.99) {
                 bmi_advise.setText("You are overweight! Are you ready for next training?");
+                float diff = weightValue - (25 *((heightValue)/100)*((heightValue)/100));
+                bmi_diff.setText(String.valueOf("You need to lose " + new DecimalFormat("##.#").format(diff) + " kg!"));
             } else {
                 bmi_advise.setText("It's serious Obese. Please contact with your doctor!");
+                float diff = weightValue - (25 *((heightValue)/100)*((heightValue)/100));
+                bmi_diff.setText(String.valueOf("You need to lose " + new DecimalFormat("##.#").format(diff) + " kg!"));
             }
 
         }
